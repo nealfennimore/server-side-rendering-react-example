272c5cea0a12eced9f3a27cf516e58d2e81459c5
Neal Fennimore <hi@neal.codes>
2017-01-25 13:58:28 -0800

    Remove blog specific meta info

--- server/templates/head/meta/home.js
+++ server/templates/head/meta/home.js
@@ -8,7 +8,7 @@ export default function homeMeta({initialState, router}) {
         <link rel="canonical" href="${url}">
         <meta name="referrer" content="origin">
 
-        <meta property="og:site_name" content="neal.codes">
+        <meta property="og:site_name" content="">
         <meta property="og:type" content="website">
         <meta property="og:title" content="${title}">
         <meta property="og:url" content="${url}">
