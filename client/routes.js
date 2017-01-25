34a8728ec8ce9c1d0051e251aaa45a2486427aa3
Neal Fennimore <hi@neal.codes>
2017-01-25 12:01:59 -0800

    Remove blog specific routes

--- client/routes.js
+++ client/routes.js
@@ -1,7 +1,6 @@
 import React, { PropTypes } from 'react';
 import {Router, Route, IndexRoute} from 'react-router';
-import { Layout, Home, Blog, Projects } from 'containers';
-import { Post, Tags } from 'components/blog';
+import { Layout, Home } from 'containers';
 import { NotFound } from 'components/common';
 import { logPageView } from 'lib/googleAnalytics';
 
@@ -10,13 +9,6 @@ import { logPageView } from 'lib/googleAnalytics';
 const ROUTES = (
     <Route path='/' component={Layout}>
         <IndexRoute component={Home} />
-
-        <Route path='blog(/page/:page)' component={Blog}>
-            <Route path='tag/:slug(/page/:tagPage)' component={Tags} />
-            <Route path=':slug' component={Post} />
-        </Route>
-
-        <Route path='/projects' component={Projects} />
 
         <Route path='*' status={404} component={NotFound} />
     </Route>
